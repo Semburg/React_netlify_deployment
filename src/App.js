e0fbb2bc2464bd24928cd1abac1c349430e25e1d
Semburg <semburg.sb@gmail.com>
2021-10-06 16:55:49 +0200

    done changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>Deployment Runs over github</h3>
+        <h2>BREAKING CHANGES</h2>
       </header>
     </div>
   );
